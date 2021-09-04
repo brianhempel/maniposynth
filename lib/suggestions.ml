04aa781ab1c44fc790d30e71a4d593753ed92eb9
Brian Hempel <bnhempel@mtu.edu>
2021-09-03 22:15:30 -0500

    next TODOs

--- lib/suggestions.ml
+++ lib/suggestions.ml
@@ -175,3 +175,11 @@ let suggestions (trace : Trace.t) (type_lookups : Typing.lookups) (final_tenv : 
       completion::restRev |> List.rev |> String.concat " "
     end
   end
+
+(* START HERE *)
+(* Revisit synth workflow *)
+(* Add missing binding should look in vises to see if name used in function position *)
+(* Simple assert value? *)
+(* "Use" in vis? *)
+(* "Use" in pat? *)
+(* Drag value into value? *)
