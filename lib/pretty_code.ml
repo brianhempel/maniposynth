c327615124a7630a3210983631da2b44c44a7433
Brian Hempel <bnhempel@mtu.edu>
2021-09-21 14:35:21 -0500

    Ravi wants to see how diffs feel…apparently all you have to do is open a diff view in VS Code and you can still edit the files and they will reload from disk

--- lib/pretty_code.ml
+++ lib/pretty_code.ml
@@ -1,13 +1,19 @@
+open Shared.Util
+
 (* Output formatted code. *)
 let output_code parsed path =
   let old_parsed = Camlboot_interpreter.Interp.parse path in
   let old_code = Shared.Formatter_to_stringifier.f Pprintast.structure old_parsed in
   let code = Shared.Formatter_to_stringifier.f Pprintast.structure parsed in
   if code <> old_code then begin
+    let old_path = String.replace ".ml" "-old.ml" path in
+    ignore @@ Unix.system @@ "cp " ^ path ^ " " ^ old_path; (* Save previous version as path-old.ml for diff. *)
     let out_chan = open_out path in
     output_string out_chan code;
     close_out out_chan;
     ignore @@ Unix.system @@ "ocamlformat --inplace --enable-outside-detected-project '" ^ path ^ "'";
     (* Turn ( ?? ) into (??) *)
-    ignore @@ Unix.system @@ "ruby -e \"File.write(ARGV[0], File.read(ARGV[0]).gsub(/\\(\\s*\\?\\?\\s*\\)/m, '(??)'))\" '" ^ path ^ "'"
+    ignore @@ Unix.system @@ "ruby -e \"File.write(ARGV[0], File.read(ARGV[0]).gsub(/\\(\\s*\\?\\?\\s*\\)/m, '(??)'))\" '" ^ path ^ "'";
+    (* Ensure diff view open *)
+    (* ignore @@ Unix.system @@ "code --diff " ^ old_path ^ " " ^ path; *)
   end
