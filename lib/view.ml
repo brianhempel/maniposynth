565689a64218b376d72bd0d50bfbd0d1c0244a18
Brian Hempel <bnhempel@mtu.edu>
2021-11-06 12:31:48 -0500

    Larger root pat/exp targets again

--- lib/view.ml
+++ lib/view.ml
@@ -732,7 +732,7 @@ and render_tv ?(show_output = true) stuff pat_opt (exp_opt : expression option) 
         local_canvas_vbs_and_returns_htmls stuff exp
       else
         [ div ~attrs:[("class", "label")] @@
-          (pat_opt |>& (fun pat -> html_of_pat ~attrs:[("class", "pat-label pat")] stuff pat ^ " = ") |> Option.to_list) @
+          (pat_opt |>& (fun pat -> html_of_pat ~attrs:[("class", "pat-label pat")] stuff pat ^ "=") |> Option.to_list) @
           [html_of_exp ~tv_root_exp:true stuff exp]
         ; html_of_values_for_exp stuff pat_opt exp
         ]
